aadccc80a4533db258137bff4cfcfa9fb09b79b3
unknown <eNTy@github.com>
2011-11-07 11:00:08 +0100

    kurzy na urovni Prepravy
    Zmeny ve frontendu:
    - p?es Link Manager oziveny tabulky aby obsahovaly nové sloupce
    - p?idány VIEW NakladyKurz, VynosyKurz, NakladySilKurz a VynosySilKurz. Náklady v?etn? kurz? (join do Preprav)
    - upraveny VIEW NakladyKC, VynosyKC, NakladyKCSilnicni a VynosyKCSilnicni. Pouzivaji nyni vyse uvedena view
    - formulare pro obe prepravy obsahuji nove pole pro Kurz USD a Kurz EUR. Pokud je vyplnen Mesic a Rok, je k dispozici tlacitko, ktere nacte kurzy z kurzovniho listku - kurzy se nenatahnou automaticky!!! pozn: to je problem pac nejde zadavat nove prepravy
    
    Signed-off-by: Tomas <Tassader@github.com>

---
+++ sql/09 PrepravaAddRates.sql
@@ -0,0 +1,38 @@
+BEGIN TRAN
+
+ALTER TABLE Preprava ADD KurzEUR real
+ALTER TABLE Preprava ADD KurzUSD real
+
+GO 
+
+UPDATE	Preprava
+SET		KurzEUR = ISNULL(EUR, 25)
+		,KurzUSD = ISNULL(USD, 20)
+FROM	Preprava
+		LEFT JOIN Kurzy ON Kurzy.Rok = Preprava.Rok AND Kurzy.Tyden = Preprava.Tyden
+
+GO
+
+ALTER TABLE Preprava ALTER COLUMN KurzEUR real NOT NULL
+ALTER TABLE Preprava ALTER COLUMN KurzUSD real NOT NULL
+GO
+
+
+ALTER TABLE PrepravaSil ADD KurzEUR real
+ALTER TABLE PrepravaSil ADD KurzUSD real
+
+GO 
+
+UPDATE	PrepravaSil
+SET		KurzEUR = ISNULL(EUR, 25)
+		,KurzUSD = ISNULL(USD, 20)
+FROM	PrepravaSil
+		LEFT JOIN Kurzy ON Kurzy.Rok = PrepravaSil.Rok AND Kurzy.Tyden = PrepravaSil.Tyden
+
+GO
+
+ALTER TABLE PrepravaSil ALTER COLUMN KurzEUR real NOT NULL
+ALTER TABLE PrepravaSil ALTER COLUMN KurzUSD real NOT NULL
+GO
+-- COMMIT
+-- ROLLBACK
