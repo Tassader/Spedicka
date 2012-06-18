e96ec691dd464a0b1cab39773e86cacbefe16085
Tomas <tassader@github.com>
2012-06-18 03:28:27 +0200

    20.0 nabidky upravena doba prepravy na varchar (aby slo zadar rozpeti dnu)

---
+++ sql/57 nabidky uprava DobaPrepravy na varchar.sql
@@ -0,0 +1,4 @@
+ALTER TABLE NabidkaKontejnery ALTER COLUMN DobaPrepravy varchar(50) NULL;
+ALTER TABLE NabidkaLCL ALTER COLUMN DobaPrepravy varchar(50) NULL;
+ALTER TABLE NabidkaAIR ALTER COLUMN DobaPrepravy varchar(50) NULL;
+GO
