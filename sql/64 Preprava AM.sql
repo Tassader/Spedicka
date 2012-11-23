a5601ad03b231cef8dc11cf16f54ca6dc93cb34c
Tomas <tassader@github.com>
2012-11-23 14:30:08 +0100

    23.1 drobne upravy u AM Trucking v SQL zmena triggeru aby byl v cisle i akronym drobna uprava leteckych nabidek

--- sql/64 Preprava AM.sql
+++ sql/64 Preprava AM.sql
@@ -198,10 +198,9 @@ BEGIN
 	-- COMMIT
 	-- 
 	UPDATE PrepravaAT SET 
-        Cislo=CAST(YEAR(GETDATE()) AS VARCHAR) + '-' + 
+        Cislo=CAST(YEAR(GETDATE()) AS VARCHAR) + '-AM' + 
             Right('000' + 
-                CAST((SELECT ISNULL(MAX(CAST(RIGHT(P.Cislo, LEN(P.Cislo)-5)AS smallint)),0)+1 FROM PrepravaAT P WHERE LEFT(P.Cislo, 4) like YEAR(GETDATE())) AS VARCHAR),
-                CASE When YEAR(GETDATE())=2011 THEN 3 ELSE 4 END)
+                CAST((SELECT ISNULL(MAX(CAST(RIGHT(P.Cislo, LEN(P.Cislo)-7)AS smallint)),0)+1 FROM PrepravaAT P WHERE LEFT(P.Cislo, 4) like YEAR(GETDATE())) AS VARCHAR),                4)
   		,KurzEUR = EUR -- ISNULL(EUR, 25)
 		,KurzUSD = USD -- ISNULL(USD, 20)
     FROM inserted
