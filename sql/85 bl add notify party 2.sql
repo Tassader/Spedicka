0cff77a010cb66a66cf543491a41544091ac4eca
Tomas <tassader@github.com>
2013-09-09 10:22:20 +0200

    33.0 upravene BL (pridano notify party 2, posuny policek, upravy textu)

---
+++ sql/85 bl add notify party 2.sql
@@ -0,0 +1,4 @@
+ALTER TABLE BL ADD NotifyParty2 nvarchar(255) NULL;
+ALTER TABLE BL ADD NotifyParty2H1 nvarchar(255) NULL;
+ALTER TABLE BL ADD NotifyParty2H2 nvarchar(255) NULL;
+ALTER TABLE BL ADD NotifyParty2H3 nvarchar(255) NULL;
