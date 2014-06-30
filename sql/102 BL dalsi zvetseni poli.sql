77b543f912b84932b6c26473cbd2cb1dd2c2168c
Tomas <tassader@github.com>
2014-06-30 18:53:39 +0200

    zvetseni policek ForDelivery v BL, pridani scriptu pro zmenu pisma v ForDelivery, POD a Place of Delivery + uprava pozice a velikosti policek

---
+++ sql/102 BL dalsi zvetseni poli.sql
@@ -0,0 +1,5 @@
+-- BL nvarchar 255->nvarchar(500)
+ALTER TABLE BL ALTER COLUMN ForDelivery nvarchar(500) NULL;
+ALTER TABLE BL ALTER COLUMN ForDeliveryH1 nvarchar(500) NULL;
+ALTER TABLE BL ALTER COLUMN ForDeliveryH2 nvarchar(500) NULL;
+ALTER TABLE BL ALTER COLUMN ForDeliveryH3 nvarchar(500) NULL;
