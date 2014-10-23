c8d483021e64f9731a0f92c58785aaa9735cc6b7
Tomas <tassader@github.com>
2014-10-23 05:31:18 +0200

    42.0 BL pridano zamceno, firmy pridano pojisteniMena

---
+++ sql/104 Firma add Pojisteni.sql
@@ -0,0 +1,5 @@
+ALTER TABLE Firma ADD PojisteniPlatneDo date NULL;
+ALTER TABLE Firma ADD PojisteniCastkaCelkem int NULL;
+ALTER TABLE Firma ADD PojisteniCastka int NULL;
+ALTER TABLE Firma ADD PojisteniMena nchar(3) SPARSE NULL;
+GO
