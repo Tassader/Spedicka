b2eeb36615e261fd2d6ed50bf91b0efcdf2faaaa
Tomas <tassader@github.com>
2015-04-02 19:28:25 +0200

    upominky -htmlspecialchars, pridano wh

--- PrimaUpominky/web/index.php
+++ PrimaUpominky/web/index.php
@@ -9,55 +9,8 @@
     </style>
 </head>
 <body>
-<H2>Chyby, které se objevily během odesílání upomínek</H2>
-<table width="100%">
-<tr>
-    <th>Datum</th>
-    <th>Text</th>
-    <th>Faktura</th>
-</tr>
-<?php
-    $db=New PDO("sqlsrv:server=192.168.88.100\PRIMA;Database=PrimaSpedicka");
-
-    $query=$db->query("select TOP 100 * from UpominkyChyby ORDER BY datum DESC");
-while ($row=$query->fetch())
-{
-?>
-<tr>
-    <td><?php echo $row['datum']?></td>
-    <td><?php echo $row['popis']?></td>
-    <td><?php echo $row['faktura']?></td>
-</tr>
-<?php
-}
-?>
-</table>
-
-
-<H2>Úspěšně odeslané upomínky</H2>
-<table width="100%">
-<tr>
-    <th>Datum</th>
-    <th>Upomínka</th>
-    <th title="První faktura, bylo-li jich více">Faktura</th>
-    <th>E-mail</th>
-</tr>
-<?php
-    //$db=New PDO("sqlsrv:server=192.168.88.100\PRIMA;Database=PrimaSpedicka");
-
-    $query=$db->query("select TOP 100 * from UpominkyOdeslane ORDER BY datum DESC");
-while ($row=$query->fetch())
-{
-?>
-<tr>
-    <td><?php echo $row['datum']?></td>
-    <td align="center"><?php echo $row['upominka']?></td>
-    <td><?php echo $row['faktura']?></td>
-    <td><?php echo $row['email']?></td>
-</tr>
-<?php
-}
-?>
-</table>
-</body>
-</html>
+<H2>Rozcestník</H2>
+<ul>
+<li><a href="prima.php">Prima Logistics</a></li>
+<li><a href="wh.php">Prima Warehousing and Distribution</a></li>
+</ul>
