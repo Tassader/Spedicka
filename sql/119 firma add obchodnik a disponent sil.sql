78a5b85cca48116c5c78314828ddf7a0c4627f30
Tomas <tassader@github.com>
2016-05-09 07:33:58 +0200

    disponent/obchodnik - odmazani lidi z druhe pobocky

--- sql/119 firma add obchodnik a disponent sil.sql
+++ sql/119 firma add obchodnik a disponent sil.sql
@@ -11,3 +11,9 @@ REFERENCES Obchodnik (ID)
 GO
 UPDATE Firma set DisponentSil=Disponent;
 UPDATE Firma set ProdejceSil=Prodejce;
+--
+UPDATE Firma SET Prodejce = NULL WHERE Prodejce IN (5,6,7,8,13,16,31,35,36,38,39) AND Prodejce = ProdejceSil;
+UPDATE Firma SET Disponent = NULL WHERE Disponent IN (5,6,7,8,13,16,31,35,36,38,39) AND Disponent = DisponentSil;
+
+UPDATE Firma SET ProdejceSil = NULL WHERE ProdejceSil IN (1,2,3,4,9,10,12,17,18,20,24,25,26,27,28,30,33,34,37) AND Prodejce = ProdejceSil;
+UPDATE Firma SET DisponentSil = NULL WHERE DisponentSil IN (1,2,3,4,9,10,12,17,18,20,24,25,26,27,28,30,33,34,37) AND Disponent = DisponentSil;
